1dcee43ce66bc8d9f268dd1e1454dacd2c7800a1
Anthony <ajclay7@gmail.com>
2021-10-27 21:16:28 -0400

    Add game loop

--- app.js
+++ app.js
@@ -16,26 +16,60 @@ const playRound = (playerSelection, computerSelection) => {
     computerSelection = computerSelection.toLowerCase();
 
     if (playerSelection === computerSelection) {
-        return "It's a tie";
+        return "tie";
     } else if (
         (playerSelection === "rock" && computerSelection === "scissors") ||
         (playerSelection === "paper" && computerSelection === "rock") ||
         (playerSelection === "scissors" && computerSelection === "paper")
      ) {
-        return `You Win! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}`
+        return `player`
     } else if (
         (playerSelection === "rock" && computerSelection === "paper") ||
         (playerSelection === "paper" && computerSelection === "scissors") ||
         (playerSelection === "scissors" && computerSelection === "rock")
     ) {
-        return `You Lose! ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}`
+        return `computer`
     }
 
 }
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
+const game = () => {
+    let playerScore = 0;
+    let computerScore = 0;
 
-console.log("Player: " + capitalize(playerSelection))
-console.log("Computer: " + capitalize(computerSelection))
-console.log(playRound(playerSelection, computerSelection));
+    while (playerScore < 5 && computerScore < 5) {
+        let playerSelection = prompt("Choose your weapon");
+        let computerSelection = computerPlay();
+
+        winner = playRound(playerSelection, computerSelection);
+
+        if (winner === "player") {
+            playerScore++;
+            console.log(`You Win! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}`);
+        } else if (winner === "computer") {
+            computerScore++;
+            console.log(`You Lose! ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}`);
+        } else if (winner === "tie") {
+            console.log("It's a tie");
+        } else {
+            console.log("Something went wrong here :( ");
+        }
+
+        console.log(`Score (Player: ${playerScore}) (Computer: ${computerScore})`);
+    }
+
+    if (playerScore === 5){
+        console.log("Winnner Winner Chicken Dinner!")
+    } else if (computerScore === 5) {
+        console.log("You've lost! We are all doomed!")
+    }
+}
+
+game();
+
+// const playerSelection = "rock";
+// const computerSelection = computerPlay();
+
+// console.log("Player: " + capitalize(playerSelection))
+// console.log("Computer: " + capitalize(computerSelection))
+// console.log(playRound(playerSelection, computerSelection));
